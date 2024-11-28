0fe1a5398f8e07164ad2c2ba11bad05073ec7b31
Evan Stoddard <evan.stoddard@ovyl.io>
2024-11-27 21:34:34 -0600

    Build script.

--- fpga/reference_design/digilent_nexys/scripts/build.tcl
+++ fpga/reference_design/digilent_nexys/scripts/build.tcl
@@ -1,21 +1,20 @@
 
-# build settings
+# Define directories
+set script_dir [file dirname [file normalize [info script]]]
+set project_dir [file normalize "${script_dir}/../"]
+set src_dir [file normalize "${project_dir}/Src"]
+set constr_dir [file normal "${project_dir}/constraints"]
+
 set design_name "digilent_nexys_reference"
 set arch "xc7"
 set board_name "Nexys 4 DDR"
 set fpga_part "xc7a100tcsg324-1"
 
-# set reference directories for source files
-set script_dir [file normalize [info script]]
-set project_dir [file normalize "${script_dir}/../../"]
-set project_src_dir [file normalize "${project_dir}/Src"]
-set project_constraints_dir [file normalize "${project_dir}/constraints"]
-
 # read design sources
-read_vhdl "${project_src_dir}/top_level.vhd"
+read_vhdl "${src_dir}/top_level.vhd"
 
 # read constraints
-read_xdc "${project_constraints_dir}/constraints.xdc"
+read_xdc "${constr_dir}/constraints.xdc"
 
 # synth
 synth_design -top "top_level" -part ${fpga_part}
@@ -26,4 +25,5 @@ place_design
 route_design
 
 # write bitstream
+file mkdir ${project_dir}/build
 write_bitstream -force "${project_dir}/build/${design_name}.bit"
